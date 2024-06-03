e226fcec5607d9e78b1457b6b153bed4a281a60a
Akari <barryofc11@gmail.com>
2024-06-03 19:42:54 +0000

    setup: initial setup.sh
    Signed-off-by: Akari <barryofc11@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# install packages
+packages() {
+	sudo apt install bc ccache neovim -y
+}
+
+# configure clang
+clang() {
+	git clone https://github.com/moekernel/clang
+	cd clang
+	chmod +x clang.sh
+	./clang.sh
+	cd ../
+	rm -rf clang
+}
+
+# idk lol.
+kernel() {
+	make mrproper
+}
+
+packages
+clang
+kernel
