6ae3daefe43d9ec8807f78d62ff457453c16ecdd
Akari <barryofc11@gmail.com>
2024-06-03 19:59:53 +0000

    setup.sh: added gcc_()
    Signed-off-by: Akari <barryofc11@gmail.com>

--- setup.sh
+++ setup.sh
@@ -15,6 +15,27 @@ clang() {
 	rm -rf clang
 }
 
+gcc_() {
+	GCC_64_DIR="$HOME/tc/aarch64-linux-android-14.0"
+	GCC_32_DIR="$HOME/tc/arm-linux-androideabi-14.0"
+
+	if ! [ -d "${GCC_64_DIR}" ]; then
+		echo "gcc not found! Cloning to ${GCC_64_DIR}..."
+		if ! git clone --depth=1 -b 14 https://github.com/ZyCromerZ/aarch64-zyc-linux-gnu ${GCC_64_DIR}; then
+			echo "Cloning failed! Aborting..."
+			exit 1
+		fi
+	fi
+
+	if ! [ -d "${GCC_32_DIR}" ]; then
+		echo "gcc_32 not found! Cloning to ${GCC_32_DIR}..."
+		if ! git clone --depth=1 -b 14 https://github.com/ZyCromerZ/arm-zyc-linux-gnueabi ${GCC_32_DIR}; then
+			echo "Cloning failed! Aborting..."
+			exit 1
+		fi
+	fi
+}
+
 kernelsu() {
 	./ksu_update.sh -t stable
 }
@@ -26,5 +47,6 @@ kernel() {
 
 packages
 clang
+gcc_
 kernelsu
 kernel
